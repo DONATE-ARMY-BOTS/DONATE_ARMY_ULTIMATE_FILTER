b5a0bfd2133446ca7586883e2410a036bd83d919
𝘿𝙊𝙉𝘼𝙏𝙀 ๛ 𝘼𝙍𝙈𝙔 🇮🇳 ™ <sanjoymandal.902137@gmail.com>
2024-09-15 20:33:14 +0530

    Add files via upload

---
+++ plugins/modules/logo.py
@@ -0,0 +1,29 @@
+
+
+from pyrogram import Client, filters, idle
+import pyrogram, asyncio, random, time
+from pyrogram.errors import FloodWait
+from pyrogram.types import *
+from info import PREFIX
+import requests
+
+
+@Client.on_message(filters.command(["logo"], PREFIX))
+async def logo(bot, msg: Message):
+    if len(msg.command) == 1:
+       return await msg.reply_text("Usage:\n\n /logo Ziyan")
+    logo_name = msg.text.split(" ", 1)[1]
+    API = f"https://api.sdbots.tech/logohq?text={logo_name}"
+    req = requests.get(API).url
+    await msg.reply_photo(
+        photo=f"{req}")
+
+@Client.on_message(filters.command(["animelogo"], PREFIX))
+async def animelogo(bot, msg: Message):
+    if len(msg.command) == 1:
+       return await msg.reply_text("Usage:\n\n /animelogo Ziyan")
+    logo_name = msg.text.split(" ", 1)[1]
+    API = f"https://api.sdbots.tech/anime-logo?name={logo_name}"
+    req = requests.get(API).url
+    await msg.reply_photo(
+        photo=f"{req}")
